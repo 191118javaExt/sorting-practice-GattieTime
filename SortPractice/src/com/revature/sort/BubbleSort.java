68e52776d397c641ea6c0c1afe01e2d220ec8e29
GattieTime <gattietime@gmail.com>
2019-12-03 21:21:30 -0500

    Merge and Bubble sorts complete

--- SortPractice/src/com/revature/sort/BubbleSort.java
+++ SortPractice/src/com/revature/sort/BubbleSort.java
@@ -3,6 +3,17 @@ package com.revature.sort;
 public class BubbleSort {
 	
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		for(int i=0; i<arr.length; i++) {
+			for( int x =1; x<arr.length-i; x++) {
+				int temp = arr[x];
+				if (arr[x]<arr[x-1]) {
+					arr[x] = arr[x-1];
+					arr[x-1] = temp;
+				}
+			}
+		}
+//		for(int i=0; i<arr.length; i++) {
+//			System.out.println(arr[i]);
+//		}
 	}
 }
--- SortPractice/src/com/revature/sort/MergeSort.java
+++ SortPractice/src/com/revature/sort/MergeSort.java
@@ -3,10 +3,47 @@ package com.revature.sort;
 public class MergeSort {
 	
 	public static void sort(int[] arr) {
-		// TODO Auto-generated method stub
+		if (arr.length < 2) {
+			return;
+		}
+		int mid = (arr.length/2);
+		int [] left = new int[mid];
+		int [] right = new int [arr.length - mid];
+		
+		for (int i = 0; i<mid; i++) {
+			left[i] = arr[i];
+		}
+		for (int i = mid; i<arr.length; i++) {
+			right[i-mid] = arr[i];
+		}
+		
+		sort(left);
+		sort(right);
+		
+		merge(arr, left, right);
 	}
 	
 	private static void merge(int[] arr, int[] left, int[] right) {
-		// TODO Auto-generated method stub
+		int r = 0;
+		int l = 0;
+		
+		
+		while(r<right.length && l<left.length) {
+			if(left[l] < right[r]) {
+				arr[l+r] = left[l];
+				l++;
+			}else {
+				arr[l+r]=right[r];
+				r++;
+			}
+		}
+		
+		for(; l<left.length; l++) {
+			arr[l+r] = left[l];
+		}
+		for(; r<right.length; r++) {
+			arr[l+r] = right[r];
+		}
+		
 	}
 }
